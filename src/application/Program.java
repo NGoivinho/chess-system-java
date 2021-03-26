d73d527f02e81896996476a5be8b2ae9683a0ab2
Natália Miranda <natalia.goivinho@gmail.com>
2021-03-26 10:52:39 -0300

    Criando classe position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello");
-
+		Position p1 = new Position(1, 2);
+		
+		System.out.println("Position: " + p1.toString());
 	}
 
 }
